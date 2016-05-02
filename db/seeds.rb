bc1d861b5273094b3337e9712d319e8414ffe420
Ross Young <ross.young12@gmail.com>
2016-05-02 10:52:24 -0500

    adds breed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Breed.create([{name: "Yorkshire Terrier"}, {name: "Dachshund"}, {name: "Beagle"}, {name: "Welsh Corgi"}])
